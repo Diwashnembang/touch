ce437836e6a9cdcc2549b299ee7f7b67aaa6e887
Diwash Nembang <diwashnembang2@gmail.com>
2024-11-15 19:50:44 +1100

    fixed erros check

--- main.go
+++ main.go
@@ -5,32 +5,28 @@ import (
 	"log"
 	"os"
 	"sync"
-	"time"
 )
 
 var wg sync.WaitGroup
 
 func main() {
-	if len(os.Args) <= 0 {
-		log.Fatal("need at lest one argument")
+	osArgs := os.Args
+	if len(osArgs) <= 1 {
+		fmt.Println(fmt.Errorf("need at lest one argument"))
 		return
 	}
-	args := os.Args[1:]
-	t1 := time.Now()
+	args := osArgs[1:]
 	for _, value := range args {
 		wg.Add(1)
 		go createFiles(value)
 	}
 	wg.Wait()
-	fmt.Println("completed in ", time.Since(t1))
 
 }
 
 func createFiles(value string) {
 	defer wg.Done()
-	// filename := fmt.Sprintf("type nul > %s", value)
 	_, err := os.Create(value)
-	// cmd := exec.Command("cmd", "/C", filename)
 	if err != nil {
 		log.Println(err)
 	}
